6907316b312dcd41d5e43c0ae86d2ac678624d61
Mahmut Karaca <mahmutkaraca95@gmail.com>
2017-09-05 21:14:16 +0300

    Split scripts into 2 - run.sh runs the program - install.sh installs dependencies

---
+++ install.sh
@@ -0,0 +1,6 @@
+pip install virtualenv
+
+virtualenv env
+source env/bin/activate
+
+pip3 install -r requirements.txt
--- run.sh
+++ run.sh
@@ -1,8 +1,1 @@
-pip install virtualenv
-
-virtualenv env
-source env/bin/activate
-
-pip3 install -r requirements.txt
-
 python3 coursePlanner.py
